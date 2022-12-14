d0811a88eada446539eb6db24036cc42c2b7e24c
Minh Duong <teobatman00@gmail.com>
2022-12-14 15:55:36 +0700

    init

---
+++ src/main/java/org/example/entity/ProductEntity.java
@@ -0,0 +1,19 @@
+package org.example.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "product")
+public class ProductEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    private String name;
+
+    private String description;
+
+
+
+}
